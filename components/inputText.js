868c48e79136ac5d69757e441b5174c58b213208
Zeppelin17 <polmonclus@outlook.com>
2022-09-26 12:08:02 +0200

    [ADD] input component

---
+++ components/inputText.js
@@ -0,0 +1,23 @@
+const InputText = ({ label, name, placeholder, onChange, value }) => {
+  return (
+    <>
+      {label && (
+        <label
+          htmlFor={name}
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          {label}
+        </label>
+      )}
+      <input
+        type="text"
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400"
+      />
+    </>
+  )
+}
+
+export default InputText
