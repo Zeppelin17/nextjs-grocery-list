8d08262f62afa6253ace8b266ca1d5a746f5b441
Zeppelin17 <polmonclus@outlook.com>
2022-10-07 06:17:30 +0200

    [UPD] update component classes

--- components/heading1.js
+++ components/heading1.js
@@ -1,5 +1,5 @@
 const Heading1 = ({ children }) => {
-  return <h1 className="text-3xl mb-4">{children}</h1>
+  return <h1 className="text-3xl mb-4 text-blue-500">{children}</h1>
 }
 
 export default Heading1
--- components/heading2.js
+++ components/heading2.js
@@ -1,5 +1,5 @@
 const Heading2 = ({ children }) => {
-  return <h2 className="text-2xl mb-3">{children}</h2>
+  return <h2 className="text-2xl mb-3 text-blue-500">{children}</h2>
 }
 
 export default Heading2
