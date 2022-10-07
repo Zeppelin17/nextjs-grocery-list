74b83b076c99749e8b3f769cdec25571ec91ec7c
Zeppelin17 <polmonclus@outlook.com>
2022-10-07 06:19:41 +0200

    [UPD] index classes and content

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,19 @@
-import { useState } from "react"
 import Head from "next/head"
 import Heading1 from "../components/heading1"
-import Heading2 from "../components/heading2"
-import InputText from "../components/inputText"
+import GroceriesManager from "../components/groceriesManager"
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState("")
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value)
-  }
-
   return (
-    <div>
+    <div className="container mx-auto px-4">
       <Head>
         <title>Grocery list app</title>
         <meta name="description" content="Grocery list app with NextJS" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Heading1>Welcome to my grocery list app</Heading1>
-        <Heading2>Welcome to my grocery list app</Heading2>
-        <InputText
-          label="Input label"
-          name="input-test"
-          placeholder="Test field"
-          onChange={handleInputChange}
-          value={inputValue}
-        />
+      <main className="">
+        <Heading1>Tu lista de la compra</Heading1>
+        <GroceriesManager />
       </main>
     </div>
   )
